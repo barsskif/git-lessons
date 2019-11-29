81de80660c7abeebd5332d2f64cffd010c2eb3c6
unknown <bars_skif@mail.ru>
2019-11-29 12:24:21 +0300

    изменил Joke

--- src/Joke.js
+++ src/Joke.js
@@ -3,8 +3,8 @@ import React from "react"
 function Joke(props) {
     return (
         <div>
-            <h3>Вопрос: {props.question}</h3>
-            <h3>Ответ: {props.punchLine}</h3>
+            <h3 style={{display: !props.question && "none"}}>Вопрос: {props.question}</h3>
+            <h3 style={{color: !props.question && "#888888"}}>Ответ: {props.punchLine}</h3>
             <hr/>
         </div>
     )
